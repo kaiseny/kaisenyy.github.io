8624472232e425ea3646dcf33a8c4620776f4d78
kaiseny <kaiyamasaki123@gmail.com>
2026-04-27 21:30:04 -1000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Kaisen Yamasaki
+    File Name: script.js
+    Date: 04/09/2026
+*/
+
+function changeImage(imageSrc, imageCaption) {
+    document.getElementById("mainImage").src = imageSrc;
+    document.getElementById("caption").textContent = imageCaption;
+}
